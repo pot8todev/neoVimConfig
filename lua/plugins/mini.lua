04448cc48920dc24cfd261db5397c43aafc91bf0
pot8todev <pedrohjesus234@gmail.com>
2025-10-16 19:08:17 -0300

    merge conflicts solved

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,1 +1,7 @@
-return { "nvim-mini/mini.icons", version = "*" }
+return {
+  { "nvim-mini/mini.comment", version = "*" },
+  { "nvim-mini/mini.pairs", version = "*" },
+  { "nvim-mini/mini.surround", version = "*" },
+  { "nvim-mini/mini.indentscope", version = "*" },
+  { "nvim-mini/mini.icons", version = "*" },
+}
